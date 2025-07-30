9749cc66369802c92872c4b55150d6690a8efd89
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-30 23:35:20 +0530

    Update errorController.js

--- Hotel_Booking_App/controllers/errorController.js
+++ Hotel_Booking_App/controllers/errorController.js
@@ -2,7 +2,7 @@
 
 exports.error404=(req,res,next)=>{
   res.statusCode=404;
-  res.render('404',{pageTitle:'Page is not found'});
+  res.render('404',{pageTitle:' Page is not found'});
     res.end();
 
 }
