0026505acb1f2cbb323af105c6d969de8fb1c85a
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-08-22 23:55:23 +0530

    lerning cookies and sessions

--- Hotel_Booking_App/controllers/authConroller.js
+++ Hotel_Booking_App/controllers/authConroller.js
@@ -1,15 +1,18 @@
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', { pageTitle: 'Login page', isLoggedIn: false });
+};
 
-exports.getLogin=(req,res,next)=>{
-  res.render('auth/login',{pageTitle:'Login page',isLoggedIn:false});
-}
-exports.postLogin=(req,res,next)=>{
+exports.postLogin = (req, res, next) => {
   console.log(req.body);
-  req.session.isLoggedIn=true;
-  res.redirect('/')
-}
-exports.postLogOut=(req,res,next)=>{
-    res.cookie("isLoggedIn",false)
-  res.redirect('/')
+  req.session.isLoggedIn = true;
+  res.redirect('/');
+};
 
-
-}
+exports.postLogOut = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect('/login');
+  });
+};
